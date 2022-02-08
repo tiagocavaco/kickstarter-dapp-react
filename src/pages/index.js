713a13126ea7e08f67cebed9cdbb6fc119e5dea2
Tiago Cavaco <tiago.cavaco@gmail.com>
2022-02-08 00:46:11 +0000

    wip: get deployed campaigns

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,31 @@
+import { useState, useEffect } from 'react';
 import { useGlobalContext } from '../context/store';
 import useContractManager from '../hooks/useContractManager';
+import { Container } from 'semantic-ui-react';
 
 const Index = () => {
   const { campaignFactoryContract } = useContractManager();
-  const { globalState, dispatch } = useGlobalContext();
+  const { globalState } = useGlobalContext();
   const { connected } = globalState;
 
+  useEffect(() => {
+    console.log('GET DEPLOYED CAMPAIGNS:', campaignFactoryContract);
+    if (campaignFactoryContract) {
+      getDeployedCampaigns();
+    }
+  }, [campaignFactoryContract]);
+
+  const getDeployedCampaigns = async () => {
+    const campaigns = await campaignFactoryContract.methods.getDeployedCampaigns().call();
+
+    console.log(campaigns);
+  }
+
   return (
-    <div className='main-app'>
+    <Container>
       <h1>This is the campaign list page!!!</h1>
-      {connected && campaignFactoryContract && <div><h4>Contract address - {campaignFactoryContract.options.address}</h4></div>}
-    </div>
+      {campaignFactoryContract && <div><h4>Contract address - {campaignFactoryContract.options.address}</h4></div>}
+    </Container>
   )
 }
 
