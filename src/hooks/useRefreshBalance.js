eeeaeb342b4361a70c1152a99ee465b092fb5033
Tiago Cavaco <tiago.cavaco@gmail.com>
2022-02-10 02:19:10 +0000

    wip: create refresh balance hook

---
+++ src/hooks/useRefreshBalance.js
@@ -0,0 +1,22 @@
+import { useGlobalContext } from '../context/store';
+import { getEtherBalance } from '../helpers/utils';
+
+const useRefreshBalance = (config = {}) => {
+  const { globalState, dispatch } = useGlobalContext();
+  const { web3, address } = globalState;
+
+  const refreshBalance = async () => {
+    const balance = await web3.eth.getBalance(address);
+      
+    dispatch({
+      type: 'SET_GLOBAL_STATE',
+      values: {
+        balance: getEtherBalance(balance),
+      },
+    });
+  }
+
+  return { refreshBalance };
+}
+
+export default useRefreshBalance;
