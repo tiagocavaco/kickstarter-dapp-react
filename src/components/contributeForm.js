f342397ad507583868d408a31a6f7abcde8b2344
Tiago Cavaco <tiago.cavaco@gmail.com>
2022-02-11 02:19:00 +0000

    wip: add contributeForm

---
+++ src/components/contributeForm.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { useGlobalContext } from '../context/store';
+import useContractManager from '../hooks/useContractManager';
+import useRefreshBalance from '../hooks/useRefreshBalance';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
+import { convertFromEtherToWei } from '../helpers/utils';
+
+const ContributeForm = (props) => {
+  const [value, setValue] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const { campaignContract } = useContractManager({ address: props.address });
+  const { refreshBalance } = useRefreshBalance();
+  const { globalState } = useGlobalContext();
+  const { connected, address } = globalState;
+
+  const onSubmit = async (e) => {
+    console.log('CONTRIBUTE FORM SUBMITTED!');
+    e.preventDefault();
+
+    setLoading(true);
+    setErrorMessage('');
+
+    try {
+      await campaignContract.methods.contribute().send({ from: address, value: convertFromEtherToWei(value) });
+      
+      props.refreshCampaignSummary();
+      refreshBalance();
+      
+      setValue('');
+    } catch (err) {
+      setErrorMessage(err.message);
+    } finally {      
+      setLoading(false);
+    }
+  }
+
+  return (
+    <Form onSubmit={onSubmit} error={!!errorMessage}>
+      <Form.Field>
+        <label>Amount to Contribute</label>
+        <Input label='ether' labelPosition='right' value={value} onChange={e => setValue(e.target.value)}></Input>
+      </Form.Field>
+
+      <Message error header='Oops!' content={errorMessage}></Message>
+
+      <Button primary disabled={!connected || !campaignContract} loading={loading} role='button'>Contribute</Button>
+    </Form>
+  )
+}
+
+export default ContributeForm;
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,6 +2,8 @@ import Web3 from "web3";
 
 export const getEtherBalance = (balance) => parseFloat(Web3.utils.fromWei(balance, "ether")).toFixed(3);
 
+export const convertFromEtherToWei = (value) => Web3.utils.toWei(value, 'ether');
+
 export const getTruncatedAddress = (address) => {
   let startStr = address.substring(0, 5);
   let endStr = address.substring(address.length - 4, address.length);
