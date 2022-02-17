b4752a749949cb7f53868bdbff8f2ba7f188dc0d
Tiago Cavaco <tiago.cavaco@gmail.com>
2022-02-17 21:19:38 +0000

    wip: deploy to gh-pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 module.exports = {
   env: {
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
     INFURA_PROVIDER_URL: process.env.INFURA_PROVIDER_URL,
     INFURA_ID: process.env.INFURA_ID,
   },
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { GlobalStore } from '../context/store';
 import Layout from "../components/layout";
 import Head from "next/head";
+import { prefix } from '../helpers/prefix';
 
 const App = ({ Component, pageProps }) => {
   return (
-    <React.StrictMode>
+    <>
       <Head>
         <title>Kickstarter</title>
+        <link rel="icon" href={`${prefix}/favicon.ico`} />
       </Head>
       <GlobalStore>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </GlobalStore>
-    </React.StrictMode>
+    </>
   );
 }
 
