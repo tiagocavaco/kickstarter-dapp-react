bafbfc5f5f6db6b3a91637980678e90d2df4c4c6
Tiago Cavaco <tiago.cavaco@gmail.com>
2022-02-17 22:04:20 +0000

    fix: css files

--- next.config.js
+++ next.config.js
@@ -3,9 +3,8 @@ require('dotenv').config();
 module.exports = {
   env: {
     PUBLIC_URL: "https://tiagocavaco.github.io/kickstarter-dapp-react",
-    assetPrefix: './',
+    assetPrefix: "https://tiagocavaco.github.io/kickstarter-dapp-react/",
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
     INFURA_PROVIDER_URL: process.env.INFURA_PROVIDER_URL,
     INFURA_ID: process.env.INFURA_ID,
   },
