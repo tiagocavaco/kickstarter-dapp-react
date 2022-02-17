806eb6353e1cdf39d99830f35228e11a0020ac6b
Tiago Cavaco <tiago.cavaco@gmail.com>
2022-02-17 22:17:41 +0000

    fix: css files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { GlobalStore } from '../context/store';
 import Layout from "../components/layout";
 import Head from "next/head";
-import { prefix } from '../helpers/prefix';
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
         <title>Kickstarter</title>
-        <link rel="icon" href={`${prefix}/favicon.ico`} />
       </Head>
       <GlobalStore>
         <Layout>
